674409c6aba8038630587d9a061800f0b5dfbefb
Aron Maijen <aron@mayen.at>
2024-01-25 00:09:05 +0100

    Added basics for sanity

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Claims;
 using UnityEngine;
 
 public class playerController : MonoBehaviour
@@ -8,7 +9,10 @@ public class playerController : MonoBehaviour
     public float speed = 12f;
     public float gravity = -9.81f;
 
-    Vector3 velocity;
+    private Vector3 velocity;
+
+    private float sanity = 100f;
+    public float sanityDepletion = 1f;
     
     void Update() {
         float x = Input.GetAxis("Horizontal");
@@ -20,6 +24,15 @@ public class playerController : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+
+        sanity -= sanityDepletion * Time.deltaTime;
+        sanity = Mathf.Clamp(sanity, 0f, 100f);
         
+        print(sanity.ToString());
+    }
+
+    public void adjustSanity(float amount) {
+        sanity += amount;
+        sanity = Mathf.Clamp(sanity, 0f, 100f);
     }
 }
