2a204bd41f9f079e20903bb3a9e31d06f675a57c
Aron Maijen <aron@mayen.at>
2024-01-25 11:35:46 +0100

    Added heartbeat audio
    heartbeatvolume is mapped to sanity meter

--- Assets/player/playerController.cs
+++ Assets/player/playerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Security.Claims;
 using UnityEngine;
 
@@ -9,10 +10,12 @@ public class playerController : MonoBehaviour
     public float speed = 12f;
     public float gravity = -9.81f;
 
-    private Vector3 velocity;
+    Vector3 velocity;
 
-    private float sanity = 100f;
+    float sanity = 100f;
     public float sanityDepletion = 1f;
+
+    public AudioSource heartbeat;
     
     void Update() {
         float x = Input.GetAxis("Horizontal");
@@ -27,12 +30,19 @@ public class playerController : MonoBehaviour
 
         sanity -= sanityDepletion * Time.deltaTime;
         sanity = Mathf.Clamp(sanity, 0f, 100f);
+
+        heartbeat.volume = map(sanity, 75f, 0f, 0f, 0.75f);
         
-        print(sanity.ToString());
+        print(heartbeat.volume.ToString());
     }
 
     public void adjustSanity(float amount) {
         sanity += amount;
         sanity = Mathf.Clamp(sanity, 0f, 100f);
+    }
+    
+    float map(float s, float a1, float a2, float b1, float b2)
+    {
+        return b1 + (s-a1)*(b2-b1)/(a2-a1);
     }
 }
